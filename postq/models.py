dd4fdf0a6da62b0d71bea1feadd8419649f4d3f5
Sean Harrison <sah@bookgenesis.com>
2020-08-23 10:16:56 -0500

    * altered Job.qname to not nullable. * added JobLog.status * Instead of Job.tasks, we have Job.workflow which represents a Workflow object. * models.Job defines queries for Jobs: .get() to get a job from the queue, .put() to put a job to the queue, and .delete() to delete a job from the queue. * models.Task defines a single workflow task * models.Workflow defines a list of tasks, from which a graph (networkx.DiGraph) is constructed. Validate that it is a DAG, and that all referenced tasks are defined. * new dependency on networkx - to provide graph data structure.

--- postq/models.py
+++ postq/models.py
@@ -1,9 +1,12 @@
 from datetime import datetime
+from typing import Any, List
 from uuid import UUID
 
-from pydantic import BaseModel, Field, ValidationError, validator
+import networkx as nx
+from pydantic import BaseModel, Field, validator
+from sqlalchemy import text
 
-from . import fields
+from . import fields, tables
 
 
 class Model(BaseModel):
@@ -11,28 +14,129 @@ class Model(BaseModel):
         return super().dict(exclude_none=exclude_none, **kwargs)
 
 
+class Task(Model):
+    """
+    A single task in a Job workflow
+
+    * name = the unique name of this task in this workflow
+    * executor = the module:name of a supported executor for this task
+    * depends = the other tasks (names) that must be completed before this task
+    * params = other parameters, such as executor-specific parameters
+    """
+
+    name: str
+    executor: str
+    depends: List[str] = Field(default_factory=list)
+    params: dict = Field(default_factory=dict)
+
+
+class Workflow(Model):
+    """
+    A workflow defines what tasks are included in a Job. The tasks define their own
+    dependencies. The resulting workflow must form a directed acyclic graph -- i.e.,
+    there cannot be a dependency loop.
+
+    * tasks - the tasks that are included in the workflow
+    * graph - the DAG (directed acyclic graph) of tasks (a networkx.DiGraph), which is
+      generated from the tasks dependencies and validated as acyclic
+    """
+
+    tasks: List[Task] = Field(default_factory=list)
+    graph: Any = Field(default_factory=nx.DiGraph)
+
+    @validator('tasks')
+    def validate_tasks(cls, value, values, **kwargs):
+        """
+        Ensure that all Task.depends are defined as Tasks.
+        """
+        errors = []
+        task_names = [task.name for task in value]
+        for task in value:
+            for depend_name in task.depends:
+                if depend_name not in task_names:
+                    errors.append(
+                        f"Task '{task.name}' depends on undefined Task '{depend_name}'."
+                    )
+        if errors:
+            raise ValueError(' '.join(errors))
+        return value
+
+    @validator('graph', always=True)
+    def validate_graph(cls, value, values, **kwargs):
+        """
+        Build the Workflow.graph from the Workflow.tasks, and ensure that the graph is
+        acyclic (a directed acyclic graph).
+        """
+        graph = nx.DiGraph()
+        for task in values.get('tasks') or []:
+            for depend_name in task.depends:
+                graph.add_edge(depend_name, task.name)
+        if not nx.is_directed_acyclic_graph(graph):
+            raise ValueError(
+                'The tasks graph must be acyclic but contains one or more cycles.'
+            )
+        return graph
+
+
 class Job(Model):
+    """
+    A single job in the Job queue.
+    """
+
     id: UUID = Field(default=None)
-    qname: str = Field(default=None)
+    qname: str
     retries: int = Field(default=1)
     queued: datetime = Field(default=None)
     scheduled: datetime = Field(default=None)
     status: str = Field(default=fields.JobStatus.queued.name)
-    tasks: dict = Field(default_factory=dict)
+    workflow: Workflow = Field(default_factory=Workflow)
     data: dict = Field(default_factory=dict)
+
+    class Config:
+        orm_mode = True
 
     @validator('status')
     def validate_job_status(cls, val, values, **kwargs):
         if val not in fields.JobStatus.__members__.keys():
-            raise ValidationError(
+            raise ValueError(
                 f'value must be one of {list(fields.JobStatus.__members__.keys())}'
             )
         return val
 
+    # queries
+    @classmethod
+    def put(cls):
+        return tables.Job.insert().returning(*tables.Job.columns)
+
+    @classmethod
+    def get(cls):
+        return text(
+            """
+            UPDATE postq.job job1 SET retries = retries - 1
+            WHERE job1.id = ( 
+                SELECT job2.id FROM postq.job job2 
+                WHERE job2.qname=:qname
+                AND job2.retries > 0
+                AND job2.scheduled <= now()
+                ORDER BY job2.queued
+                FOR UPDATE SKIP LOCKED LIMIT 1 
+            )
+            RETURNING job1.*;
+            """.strip()
+        )
+
+    @classmethod
+    def delete(cls):
+        return tables.Job.delete()
+
 
 class JobLog(Model):
+    """
+    A logged Job to maintain a historical record of Jobs that have been completed.
+    """
+
     id: UUID
-    qname: str = Field(default=None)
+    qname: str
     retries: int
     queued: datetime
     scheduled: datetime
@@ -42,10 +146,17 @@ class JobLog(Model):
     logged: datetime = Field(default=None)
     errors: dict = Field(default_factory=dict)
 
+    class Config:
+        orm_mode = True
+
     @validator('status')
     def validate_job_status(cls, val, values, **kwargs):
         if val not in fields.JobStatus.__members__.keys():
-            raise ValidationError(
+            raise ValueError(
                 f'value must be one of {list(fields.JobStatus.__members__.keys())}'
             )
         return val
+
+
+class Queue(Model):
+    qname: str = Field(default=None)
--- postq/tables.py
+++ postq/tables.py
@@ -9,13 +9,14 @@ Job = Table(
     'job',
     metadata,
     Column('id', UUID, primary_key=True, server_default=text("gen_random_uuid()")),
-    Column('qname', String, nullable=True),
-    Column('retries', SmallInteger, server_default=text("1")),
+    Column('qname', String, nullable=False, index=True),
+    Column('retries', SmallInteger, server_default=text("1"), index=True),
     Column(
         'queued',
         DateTime(timezone=True),
         nullable=False,
         server_default=text('current_timestamp'),
+        index=True,
     ),
     Column(
         'scheduled',
@@ -24,27 +25,27 @@ Job = Table(
         server_default=text('current_timestamp'),
     ),
     Column('status', String, nullable=False),
-    Column('tasks', JSONB, server_default=text("'{}'::jsonb")),
+    Column('workflow', JSONB, server_default=text("'{}'::jsonb")),
     Column('data', JSONB, server_default=text("'{}'::jsonb")),
 )
 
-# JobsLog = log of jobs that have been completed
+# JobLog = log of jobs that have been completed
 JobLog = Table(
     'job_log',
     metadata,
     Column('id', UUID),
-    Column('qname', String, nullable=True),
+    Column('qname', String, nullable=False),
     Column('retries', SmallInteger),
     Column('queued', DateTime(timezone=True), nullable=False),
     Column('scheduled', DateTime(timezone=True), nullable=False),
-    Column('status', String, nullable=False, default=fields.JobStatus.queued.name),
     Column(
         'logged',
         DateTime(timezone=True),
         nullable=False,
         server_default=text('current_timestamp'),
     ),
-    Column('tasks', JSONB, server_default=text("'{}'::jsonb")),
+    Column('status', String, nullable=False, default=fields.JobStatus.queued.name),
+    Column('workflow', JSONB, server_default=text("'{}'::jsonb")),
     Column('data', JSONB, server_default=text("'{}'::jsonb")),
     Column('errors', JSONB, server_default=text("'{}'::jsonb")),
 )
