f0bf503a0c01f9168efa48a975760e6bec263976
Sean Harrison <sah@kruxia.com>
2021-11-15 20:28:08 -0600

    0.2.4

--- buildpush.sh
+++ buildpush.sh
@@ -7,6 +7,6 @@ TAG=$(python setup.py --version)
 docker-compose build postq
 docker tag postq_postq kruxia/postq:$TAG
 docker tag postq_postq kruxia/postq
-docker push kruxia/postq
 docker push kruxia/postq:$TAG
+docker push kruxia/postq
 
