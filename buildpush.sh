0e145793051a68f4e93cce331da093bbd0880b42
Sean Harrison <sah@kruxia.com>
2021-11-16 21:02:38 -0600

    0.2.5 - better docker buildpush.sh

--- buildpush.sh
+++ buildpush.sh
@@ -3,10 +3,14 @@ set -eux
 
 cd `dirname $0`; 
 TAG=$(python setup.py --version)
+SHA=$(git rev-parse --short HEAD)
 
 docker-compose build postq
+docker tag postq_postq kruxia/postq:$SHA
 docker tag postq_postq kruxia/postq:$TAG
-docker tag postq_postq kruxia/postq
+docker tag postq_postq kruxia/postq:latest
+docker push kruxia/postq:$SHA
 docker push kruxia/postq:$TAG
-docker push kruxia/postq
-
+docker push kruxia/postq:latest
+docker image rm kruxia/postq:$TAG
+docker image rm kruxia/postq:$SHA
