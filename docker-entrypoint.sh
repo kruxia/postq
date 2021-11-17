d616a34ee96d4153a8dd2376d30f50dab6066185
Sean Harrison <sah@kruxia.com>
2021-11-16 20:58:11 -0600

    0.2.5 - remove the need for POSTGRES_* env vars, just DATABASE_URL

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 set -eux
 
-DATABASE_URL=postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:5432/${POSTGRES_DB}
-
 until psql $DATABASE_URL -c '\l'; do
     >&2 echo "Waiting for postgres..."
     sleep 1
